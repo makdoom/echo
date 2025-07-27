90eb88ffc30ba90b3cafe63d04ff411e29106231
Makdoom Shaikh <makshaikh99@gmail.com>
2025-07-27 16:12:27 +0530

    feat: Add login feature

--- apps/frontend/src/components/LoginForm.tsx
+++ apps/frontend/src/components/LoginForm.tsx
@@ -11,9 +11,16 @@ import {
 } from "./ui/form";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { Link } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
+import { useSignIn } from "@clerk/clerk-react";
+import { toast } from "sonner";
+import { useState } from "react";
+import { Loader } from "lucide-react";
 
 const LoginForm = () => {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const { isLoaded, signIn, setActive } = useSignIn();
+  const navigate = useNavigate();
   const form = useForm<authTypes.LoginSchemaType>({
     resolver: zodResolver(authTypes.LoginSchema),
     defaultValues: {
@@ -22,7 +29,35 @@ const LoginForm = () => {
     },
   });
 
-  const onSubmit = () => {};
+  const onSubmit = async (data: authTypes.LoginSchemaType) => {
+    if (!isLoaded) return;
+
+    try {
+      setIsAuthenticating(true);
+      let result = await signIn.create({
+        identifier: data.email,
+        password: data.password,
+      });
+      if (result.status == "complete") {
+        await setActive({ session: result.createdSessionId });
+        navigate({ to: "/chat" });
+      } else {
+        toast.error(
+          "Login failed, Please check your credentials and try again"
+        );
+      }
+    } catch (error: any) {
+      console.log(JSON.stringify(error, null, 2));
+      toast.error(
+        error.errors[0]?.message ||
+          "An unexpected error occurred. Please try again."
+      );
+    } finally {
+      setIsAuthenticating(false);
+    }
+  };
+
+  if (!isLoaded) return;
 
   return (
     <Form {...form}>
@@ -34,7 +69,12 @@ const LoginForm = () => {
             <FormItem>
               <FormLabel>Email</FormLabel>
               <FormControl>
-                <Input {...field} autoFocus placeholder="johndoe@gmail.com" />
+                <Input
+                  {...field}
+                  disabled={isAuthenticating}
+                  autoFocus
+                  placeholder="johndoe@gmail.com"
+                />
               </FormControl>
 
               <FormMessage />
@@ -49,15 +89,25 @@ const LoginForm = () => {
             <FormItem>
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input type="password" placeholder="******" {...field} />
+                <Input
+                  type="password"
+                  disabled={isAuthenticating}
+                  placeholder="******"
+                  {...field}
+                />
               </FormControl>
 
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit" className="w-full py-5">
+        <Button
+          type="submit"
+          className="w-full py-5"
+          disabled={isAuthenticating}
+        >
           Login
+          {isAuthenticating && <Loader className="animate animate-spin" />}
         </Button>
       </form>
 
--- apps/frontend/src/routes/chat/index.tsx
+++ apps/frontend/src/routes/chat/index.tsx
@@ -1,9 +1,25 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/chat/")({
   component: Chat,
 });
 
 function Chat() {
-  return <div>Hello "/chat/"!</div>;
+  const { isLoaded, user } = useUser();
+  const { signOut } = useAuth();
+  const navigate = useNavigate();
+
+  console.log(user);
+  const handleLogout = async () => {
+    await signOut(() => navigate({ to: "/" }));
+  };
+  if (!isLoaded) return;
+  return (
+    <div>
+      Hello "/chat/"!
+      <Button onClick={handleLogout}>Logout</Button>
+    </div>
+  );
 }
